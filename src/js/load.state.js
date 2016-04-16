ec4484283df87d09c86c9ed3379149a631f91436
Reik Val <guardian_de_las_sombras@hotmail.com>
2016-04-16 15:30:07 +0200

    Player basic graphic

--- src/js/load.state.js
+++ src/js/load.state.js
@@ -3,7 +3,7 @@ export class LoadState extends Phaser.State {
     this.loadingText = this.game.add.bitmapText(this.game.world.centerX, this.game.world.centerY, 'carrier_command', "Loading...", 18);
     this.loadingText.anchor.setTo(0.5);
 
-    this.load.spritesheet('hero', require('../images/hero.png'), 64, 64, 16);
+    this.load.spritesheet('hero', require('../images/hero.png'), 32, 32, 2);
   }
 
   create () {
